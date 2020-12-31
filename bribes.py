0500c53c445ac8c8841cc55ed6e4170da7f56c01
mhcassiem <mhcassiem@gmail.com>
2021-01-01 00:18:31 +0200

    completed

--- bribes.py
+++ bribes.py
@@ -1,3 +1,14 @@
-
 def minimumBribes(q):
-    return 3
+    bribed = [0] * len(q)
+    i = 0
+    while i < len(q):
+        if q[i] != i + 1 and q[i] > q[i + 1]:
+            bribed[q[i]-1] += 1
+            q[i], q[i + 1] = q[i + 1], q[i]
+            i = i - 1 if i > 0 else i
+        else:
+            i += 1
+    if all(item <= 2 for item in bribed):
+        print(sum(bribed))
+        return
+    print('Too chaotic')
--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ class Case0_1(unittest.TestCase):
         exp_out = 3
         self.assertEqual(exp_out, out)
 
+
 class Case0_2(unittest.TestCase):
     def test_something(self):
         inp = [2, 5, 1, 3, 4]
@@ -17,12 +18,28 @@ class Case0_2(unittest.TestCase):
         self.assertEqual(exp_out, out)
 
 
-# class Case1_0(unittest.TestCase):
-#     def test_something(self):
-#         inp = [5, 1, 2, 3, 7, 8, 6, 4]
-#         out = minimumBribes(inp)
-#         exp_out = 'Too chaotic'
-#         self.assertEqual(exp_out, out)
+class Case1_0(unittest.TestCase):
+    def test_something(self):
+        inp = [5, 1, 2, 3, 7, 8, 6, 4]
+        out = minimumBribes(inp)
+        exp_out = 'Too chaotic'
+        self.assertEqual(exp_out, out)
+
+
+class Case1_1(unittest.TestCase):
+    def test_something(self):
+        inp = [1, 2, 5, 3, 7, 8, 6, 4]
+        out = minimumBribes(inp)
+        exp_out = 7
+        self.assertEqual(exp_out, out)
+
+
+class Case2_0(unittest.TestCase):
+    def test_something(self):
+        inp = [1, 2, 5, 3, 4, 7, 8, 6]
+        out = minimumBribes(inp)
+        exp_out = 4
+        self.assertEqual(exp_out, out)
 
 
 if __name__ == '__main__':
