d31d563a9e557384f543fc9f16d28ffb653f6162
mhcassiem <mhcassiem@gmail.com>
2020-12-31 20:54:18 +0200

    more tests

--- tests.py
+++ tests.py
@@ -2,12 +2,27 @@ import unittest
 from bribes import minimumBribes
 
 
-class Case0(unittest.TestCase):
+class Case0_1(unittest.TestCase):
     def test_something(self):
         inp = [2, 1, 5, 3, 4]
         out = minimumBribes(inp)
         exp_out = 3
         self.assertEqual(exp_out, out)
+
+class Case0_2(unittest.TestCase):
+    def test_something(self):
+        inp = [2, 5, 1, 3, 4]
+        out = minimumBribes(inp)
+        exp_out = 'Too chaotic'
+        self.assertEqual(exp_out, out)
+
+
+# class Case1_0(unittest.TestCase):
+#     def test_something(self):
+#         inp = [5, 1, 2, 3, 7, 8, 6, 4]
+#         out = minimumBribes(inp)
+#         exp_out = 'Too chaotic'
+#         self.assertEqual(exp_out, out)
 
 
 if __name__ == '__main__':
