500849c5cb208478516e31e3c431499027f49002
mhcassiem <mhcassiem@gmail.com>
2020-12-31 20:09:44 +0200

    update main and bribe function

--- bribes.py
+++ bribes.py
@@ -1,3 +1,3 @@
 
 def minimumBribes(q):
-    pass
+    return q
--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from bribes import minimumBribes
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    t = int(input())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for t_itr in range(t):
+        n = int(input())
+
+        q = list(map(int, input().rstrip().split()))
+        print(minimumBribes(q))
